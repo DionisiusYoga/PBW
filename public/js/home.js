55183947f7ca95c67f50f6e0f9274a54f909e496
DionisiusYoga <6181901037@student.unpar.ac.id>
2022-06-09 20:30:43 +0700

    js testing

---
+++ public/js/home.js
@@ -0,0 +1,22 @@
+var testPost = [
+    {
+        id:1,
+        title:"Test title",
+        date:Date.now(),
+        lock_by:null,
+        authorId:10,
+        categoryId:1,
+        content:"Content Content Content Content Content Content Content Content Content"
+    },
+    {
+        id:2,
+        title:"Test title",
+        date:Date.now(),
+        lock_by:null,
+        authorId:10,
+        categoryId:1,
+        content:"Content2 Content2 Content2 Content2 Content2 Content2 Content2 Content2 Content2"
+    }
+]
+
+var threads = testPost
