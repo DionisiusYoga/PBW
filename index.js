0b4df8ebb4bc454adc71cd932d4a27c903cae017
DionisiusYoga <6181901037@student.unpar.ac.id>
2022-06-14 17:02:50 +0700

    3

--- index.js
+++ index.js
@@ -1,5 +1,26 @@
 const express = require("express")
 const app = express()
+const mysql = require("mysql")
+
+const pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    database: 'tugasbesar'
+});
+
+const dbConnect = () => {
+    return new Promise((resolve,reject) => {
+        pool.getConnection((err,conn) => {
+            if(err){
+                reject(err);
+            }
+            else{
+                resolve(conn);
+                
+            }
+        })
+    })
+}
 
 app.listen(8080)
 app.set('view engine', "ejs")
